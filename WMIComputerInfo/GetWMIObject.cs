243158205df178b287796ec2d83245ad0505445b
Michael LoNigro <mvlonigro@gmail.com>
2015-02-19 22:09:30 -0700

    Initial commit

---
+++ WMIComputerInfo/GetWMIObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using System.Threading.Tasks;
+using WMIComputerInfo.WMIDataModels;
+
+namespace WMIComputerInfo
+{
+    public class GetWMIObject
+    {
+        private PowerShell _ps = PowerShell.Create();
+
+        public IEnumerable<ComputerSystem> GetWin32_ComputerSystem()
+        {
+            var returnList = new List<ComputerSystem>();
+            using (var runspace = RunspaceFactory.CreateRunspace()) {
+                runspace.Open();
+                this._ps.Runspace = runspace;
+                IEnumerable<PSObject> psObjects = _ps.AddCommand("get-wmiobject")
+                    .AddArgument("win32_computersystem")
+                    .Invoke();
+
+                returnList.AddRange(psObjects.Select(pso => new ComputerSystem(pso)));
+            }
+
+            return returnList;
+        }
+
+
+    }
+}
---
+++ WMIComputerInfo/WMIDataModels/ComputerSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMIComputerInfo.WMIDataModels
+{
+    public class ComputerSystem
+    {
+        public string Domain { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Name { get; set; }
+        public string PrimaryOwnerName { get; set; }
+        public string TotalPhysicalMemory { get; set; }
+
+        public ComputerSystem(PSObject psObject)
+        {
+            this.Domain = psObject.Members["Domain"].Value.ToString();
+            this.Manufacturer = psObject.Members["Manufacturer"].Value.ToString();
+            this.Model = psObject.Members["Model"].Value.ToString();
+            this.Name = psObject.Members["Name"].Value.ToString();
+            this.PrimaryOwnerName = psObject.Members["PrimaryOwnerName"].Value.ToString();
+            this.TotalPhysicalMemory = psObject.Members["TotalPhysicalMemory"].Value.ToString();
+        }
+    }
+}
